0a4078b9dd2ecd06c691b75a6c2e9f9ee4c4a1b5
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-12-11 22:28:18 -0600

    promos carousel v2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,44 +1,52 @@
 import data from '../data/data'
-import {FiChevronRight, FiChevronLeft} from 'react-icons/fi'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { getPromos } from "../Redux/actions";
+import { getMangas, getPromos } from "../Redux/actions";
 
 export const Carousel = () => {
     const [activeSlider, setActiveSlider] = useState(1);
     const dispatch = useDispatch();
     const promos = useSelector((state) => state.promos);
 
-    useEffect (()=>{
-        dispatch(getPromos());
+    useEffect(() => {
+        dispatch(getPromos())
     }, [dispatch])
-    
+
     const prevSliderHandler = () => {
-            setActiveSlider(activeSlider === promos.length - 1 ? 0 : activeSlider + 1)
-        }
+        setActiveSlider(activeSlider === promos.length - 1 ? 0 : activeSlider + 1)
+    }
 
     const nextSliderHandler = () => {
-            setActiveSlider(activeSlider === 0 ? promos.length -1 : activeSlider - 1)
-        }
-        
+        setActiveSlider(activeSlider === 0 ? promos.length - 1 : activeSlider - 1)
+    }
+
     return (
         <div className='w-full mt-2' >
             {promos.map((item, index) => {
                 const { name, description, categories } = item
                 return <div key={index} className={activeSlider === index ? 'flex justify-between items-center' : 'hidden'}>
-                    <button className='text-6xl absolute left-0 bg-white rounded-full text-blue-600 hover:bg-blue-600 hover:text-white' 
-                            onClick={prevSliderHandler}>
-                        <FiChevronLeft/>
+                    <button className='text-6xl absolute left-0 bg-white rounded-full text-blue-600 hover:bg-blue-600 hover:text-white'
+                        onClick={prevSliderHandler}>
+                        <FiChevronLeft />
                     </button>
                     <div className='text-3xl w-full'>
-                        <img src={'https://cdn.pixabay.com/photo/2017/12/20/16/49/christmas-3030279__340.jpg'} alt='' className='h-80 w-screen'/>
-                        <h5 className='text-center'>
-                            <b className='text-purple-700'>{name.toUpperCase()}</b> : {description} on our volumes of the categories <b className='text-purple-700'>{categories[0].title.toUpperCase()}</b> 
-                        </h5>
+                        <div className='grid grid-cols-3 content-evenly justify-items-center bg-[url(https://img.freepik.com/fotos-premium/arreglo-navideno-decorativo-espacio-copia_23-2149155894.jpg?w=740)] bg-no-repeat bg-cover bg-center bg-fixed h-64'>
+
+                            <img src={'https://media.kitsu.io/manga/poster_images/2730/small.jpg'} alt='' className='h-56 align-middle rounded-full grayscale ml-10' />
+
+                            <h5 className='text-center content-center mt-14 text-black'>
+                                <b className='text-purple-700'>{name.toUpperCase()}</b> : {description} on our volumes of the categories <b className='text-purple-700'>{categories[0].title.toUpperCase()}</b>
+                            </h5>
+
+                            <img src={'https://media.kitsu.io/manga/poster_images/1401/small.jpg'} alt='' className='h-56 align-middle rounded-full grayscale mr-10' />
+                        </div>
+
+
                     </div>
-                    <button className='text-6xl absolute right-0 bg-white rounded-full text-blue-600 hover:bg-blue-600 hover:text-white' 
-                            onClick={nextSliderHandler}>
-                        <FiChevronRight/>
+                    <button className='text-6xl absolute right-0 bg-white rounded-full text-blue-600 hover:bg-blue-600 hover:text-white'
+                        onClick={nextSliderHandler}>
+                        <FiChevronRight />
                     </button>
                 </div>
             })}
