92b1c06f5c1d9f20ecb7da7bc4666672db670740
IvanaSDW <ivanasdw@gmail.com>
2022-12-12 11:22:32 -0500

    removed unused firestoreservice.js

--- src/domain/firestoreService.js
+++
@@ -1,9 +0,0 @@
-import { getFirestore } from 'firebase/firestore';
-import firebase from 'firebase/compat/app';
-import { firebaseConfig } from './firebaseConfig';
-
-console.log('About to initialize Firebase app..');
-const app = firebase.initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const firestore = getFirestore(app);
